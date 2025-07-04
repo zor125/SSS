7086906357c110478b8f89b4b84e3edeadf430f6
Jun's Macbook <zzor125@gmail.com>
2025-07-04 13:47:28 +0900

    button first

--- App.js
+++ App.js
@@ -1,15 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// TouchableOpacity 컴포넌트 임포트
+import { TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
+  //앱 시작
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+  //차량 사용 버튼
+  <TouchableOpacity style= {styles.circleButton}>
+    <Text style={styles.buttonText}>사용합니다</Text>
+  </TouchableOpacity>
+
+
+  //스테이터스 바
+  <StatusBar style="auto" />
+  </View>
   );
 }
 
+
+
+
+//styleSheet 시작
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -17,4 +31,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  circleButton: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: '#007AFF',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
+
+
