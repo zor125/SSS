ad7c884c551fe6ffb4bf1935fe0904f28e1aa14b
Jun's Macbook <zzor125@gmail.com>
2025-07-04 14:31:00 +0900

    time calculator

--- App.js
+++ App.js
@@ -2,8 +2,21 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 // TouchableOpacity 컴포넌트 임포트
 import { TouchableOpacity } from 'react-native';
+// 시작시간 종료시간 설정 컴포넌트 임포트
+import { CircularSlider } from 'react-native-circular-slider';
+import { useState } from 'react';
+
 
 export default function App() {
+  //시작시간 종료시간 기능 각도 상수 선언
+  const [startAngle, setStartAngle] = useState(0);
+  const [angleLength, setAngleLength] = useState(60);//60도 기본 설정
+  //각도에 따른 시간 상수 선언
+  const startHour = Math.floor(startAngle / 15); // 15도마다 한 시간, 시작시간
+  const endHour = Math.floor((startAngle + angleLength) / 15); // 종료시간
+
+
+  //리턴 시작
   return (
   //앱 시작
     <View style={styles.container}>
@@ -13,6 +26,23 @@ export default function App() {
     <Text style={styles.buttonText}>사용합니다</Text>
   </TouchableOpacity>
 
+  //시작시간 종료시간 버튼
+  <CircularSlider
+    value={startAngle}
+    onChange={setStartAngle}
+    angleLength={angleLength}
+    onAngleLengthChange={setAngleLength}
+    strokewidth={10}//선분 두께
+    buttonStrokeWidth={20}//버튼 선분 두께
+    openingRadian={Math.PI / 4} //시작 각도
+    buttonRadius={15} //버튼 반지름
+    radius={70} //슬라이더 반지름
+  />
+
+  //시작시간 종료시간 텍스트
+  <Text style={styles.timeText}>
+    시작시간: {startHour}시 ~ 종료시간: {endHour}시
+  </Text>
 
   //스테이터스 바
   <StatusBar style="auto" />
@@ -47,6 +77,11 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold',
   },
+
+  timeText: {
+    fontSize: 18,
+    marginTop: 20,
+  }
 });
 
 
